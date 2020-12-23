b1151fbbc4229881147c4c9af9515b00317b4805
a.cherdantceva <>
2020-12-23 18:47:27 +0300

    rename json -> rapidjson

--- rapidjson/document-wrapper.h
+++ rapidjson/document-wrapper.h
@@ -5,7 +5,7 @@
 // Bug in gcc 4.9.0 on ARM with ARM instruction set, -O2 causes the bug
 // This gcc version cannot use #pragma GCC push_options/pop_options as well. https://gcc.gnu.org/bugzilla/show_bug.cgi?id=59884
 
-#include "json/rapidjson.h"
+#include "rapidjson.h"
 
 #if defined(__arm__) && defined(RAPIDJSON_GNUC) && RAPIDJSON_GNUC >= RAPIDJSON_VERSION_CODE(4,9,0) && RAPIDJSON_GNUC < RAPIDJSON_VERSION_CODE(5,0,0)
     #pragma GCC optimize ("O1")
